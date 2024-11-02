cebc6f58e242c2ea3f27ace8a2f34a5762d515d5
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-02 22:58:14 +0600

    play with ganeric

--- module-2/src/2.3.ts
+++ module-2/src/2.3.ts
@@ -18,4 +18,34 @@ console.log(mentrorse);
 type GenaricTuple<x,y> =[x, y];
 const tupole:GenaricTuple<string,number>=['mr.x',9];
 console.log(tupole);
+
+// --------Try to solve few problem with genaric 
+// Generic function to remove duplicates
+// function removeDuplicates<T>(arr: T[]): T[] {
+//     // Use a Set to filter out duplicate values
+//     return Array.from(new Set(arr));
+// }
+
+// // Test cases
+// const numbers = [1, 2, 2, 3, 4, 4, 5];
+// const uniqueNumbers = removeDuplicates(numbers);
+// console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
+
+// const strings = ["apple", "banana", "apple", "orange", "banana"];
+// const uniqueStrings = removeDuplicates(strings);
+// console.log(uniqueStrings); // Output: ["apple", "banana", "orange"]
+
+function removeDup<T>(arr: T[]): T[] {
+    return Array.from(new Set(arr));
+}
+// test case 
+const numbers:number[]=[1, 2, 2, 3, 4, 4, 5];
+const result:number[] =removeDup(numbers);
+console.log('this is result for removeDip-number',result);
+
+// test with string array 
+const stringsAr:string[] = ["apple", "banana", "apple", "orange", "banana"];
+const resultStr:string[]=removeDup(stringsAr);
+console.log(resultStr);
+
 }
