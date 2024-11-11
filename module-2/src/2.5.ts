34ff847cda1ad9b6fb3d14724b69a248b8003c9e
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-11 21:38:53 +0600

    average

---
+++ module-2/src/2.5.ts
@@ -0,0 +1,13 @@
+{
+    // 
+
+
+    function sumArray(nums: number[]): number {
+
+        return nums.reduce((acc, num) => acc + num, 0);
+    }
+
+    const result = (sumArray([1, 2, 3, 4, 5]));
+    console.log(result);
+    // 
+}
