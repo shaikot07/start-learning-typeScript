0a3dd1c39629a05079e8eb164b4c139a4e606003
shaikot07 <saifulislamshaikat007@gmail.com>
2024-11-26 23:20:44 +0600

    forloop in typescript

--- module1/src/test.ts
+++ module1/src/test.ts
@@ -4,4 +4,12 @@ const apple:string="this is a apple ";
 const numberP:number=111
 
 
+// typescript for loop 
+const fruits = ["apple", "banana", "orange"];
+for (const fruit of fruits) {
+  console.log(fruit);
+}
+
+
+
 console.log(test,apple,numberP);
